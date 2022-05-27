e20bcadbbfe77efa11c091bd007059e331038da0
Adrienne <100542795+ampando@users.noreply.github.com>
2022-05-26 17:33:30 -0700

    Initial commit

---
+++ __tests__/business.test.js
@@ -0,0 +1,27 @@
+import Template from '../src/js/BUSINESS.js'
+
+describe('Template', () => {
+
+  test('should do ___', () => {
+    const example = exam;
+    expect().toEqual();
+  });
+
+})
+
+
+
+/*describe('Rectangle', () => {
+
+  let rectangle;
+
+  beforeEach(() => {
+    rectangle = new Rectangle(3,5);
+  });
+
+  test('should correctly create a rectangle object using two sides', () => {
+    expect(rectangle.side1).toEqual(3);
+    expect(rectangle.side2).toEqual(5);
+  });
+
+});*/
